d265557ab7b061b84ba8b9720a71d579fa0024c5
mannatgupta146 <mannatgupta146@gmail.com>
2025-11-13 19:06:22 +0530

    created

---
+++ script.js
@@ -0,0 +1,39 @@
+// Level 2 – Functional Thinking & Logic Tasks (Intermediate)
+
+// Write a higher-order function runTwice(fn) that takes another function and executes it two times.
+
+
+
+// Create one pure function that always returns the same output for a given input, and one impure function using a global variable.
+
+
+
+// Write a function that uses object destructuring inside parameters to extract and print name and age.
+
+
+
+// Demonstrate the difference between normal function and arrow function when used as object methods (the this issue).
+
+
+
+// Given an array of numbers, use map() to create a new array where each number is squared.
+
+
+
+// Use filter() to get only even numbers from an array.
+
+
+
+// Use reduce() to find the total salary from an array of numbers [1000, 2000, 3000].
+
+
+
+// Create an array of names and use some() and every() to test a condition (e.g., all names longer than 3 chars).
+
+
+
+// Create an object user and test the behavior of Object.freeze() and Object.seal() by adding/changing keys.
+
+
+
+// Create a nested object (user → address → city) and access the city name inside it.
